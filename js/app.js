d7fc28d33fbab897271e752a037c1ab9a932b694
Ian Mukunya <imukunya@safaricom.co.ke>
2020-06-10 08:15:38 +0300

    name display fix - math round off fix

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
 function DayWeekBorn(CC,YY,MM,DD,NameSpaces){
-var dateObj = date.split("/");
-
-
-var d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
-
-	return NameSpaces[d];
+	var d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
+	console.log(d);
+	var day = Math.round(d);
+	return NameSpaces[day];
 
 }
